1b0e5016a5a1695078b1c5d4ad1464c6c3a1f7fb
Seongryool Wee <66999675+s-ryuri@users.noreply.github.com>
2021-09-15 15:15:40 +0900

    Create subJava.java

---
+++ Android/홍드로이드3강/subJava.java
@@ -0,0 +1,22 @@
+package com.example.thirdclass;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class subActivity extends AppCompatActivity {
+    private TextView tv_sub;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub);
+
+        tv_sub = findViewById(R.id.tv_sub);
+        Intent intent = getIntent();
+        String str = intent.getStringExtra("str");
+
+        tv_sub.setText(str);
+    }
+}
